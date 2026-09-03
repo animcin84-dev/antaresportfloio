9c6efcc099a68720600b820742fa9de05ecbf90d
animcin84-dev <animcin84@gmail.com>
2026-09-03 09:16:14 +0500

    fix: load V7 overflow corrections after base styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { I18nProvider } from './i18n'
 import './styles.css'
+import './v7-overflow-fix.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
